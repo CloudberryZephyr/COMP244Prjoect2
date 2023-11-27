1bfbf032b2c9ff686a7bf63ab603e72106a27ede
Rvz4 <126510603+Rvz4@users.noreply.github.com>
2023-11-27 17:24:41 -0500

    Update project2_trigger2.sql

--- project2_trigger2.sql
+++ project2_trigger2.sql
@@ -1,19 +1,20 @@
-
-# Trigger 2. Write an SQL trigger called updateNVols that 
+# Trigger 2. 2.	Write an SQL trigger called updateNVols that 
 # detects when a volunteer volunteers for a timeslot and 
 # decrements both the number of volunteers needed for the timeslot 
 # and the number of volunteers needed for the project. [8 points]
+drop trigger if exists updateNVols;
+
 delimiter @@
 
 create trigger updateNVols
-after update on projvolunteers
+after insert on projvolunteers
 
 for each row begin
 
 update timeslot set nVolneeded = nVolneeded - 1
-where Day = OLD.Day AND startingTime = OLD.startingTime AND projID = OLD.projID;
+where Day = NEW.Day AND startingTime = NEW.startingTime AND projID = NEW.projID;
 
 update project set nVolneeded = nVolneeded - 1
-where projID = OLD.projID;
+where projID = NEW.projID;
 
 end @@
